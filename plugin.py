3cd89a25a5f834cb83f60693be05ea33ec3384b0
Jean-Claude Computing <jeanclaude.computing@gmail.com>
2024-11-10 18:44:00 +0100

    Reformat file with Ruff

--- plugin.py
+++ plugin.py
@@ -37,76 +37,143 @@
     </params>
 </plugin>
 """
+
 import traceback
 import midea_beautiful
-from midea_beautiful import connect_to_cloud, appliance_state, DEFAULT_APPKEY, DEFAULT_HMACKEY, DEFAULT_IOTKEY
+from midea_beautiful import (
+    connect_to_cloud,
+    appliance_state,
+    DEFAULT_APPKEY,
+    DEFAULT_HMACKEY,
+    DEFAULT_IOTKEY,
+)
 
-with open('/var/log/domoimport.txt', 'a') as f:
-   f.write("starting midea plugin\n")
-try: 
+with open("/var/log/domoimport.txt", "a") as f:
+    f.write("starting midea plugin\n")
+try:
     import Domoticz
     import subprocess
 except Exception as err:
     tbstr = traceback.format_exc()
-    with open('/var/log/domoimport.txt', 'a') as f:
-       f.write(tbstr)
-       f.write("\n")
-    exit(1);
-    
+    with open("/var/log/domoimport.txt", "a") as f:
+        f.write(tbstr)
+        f.write("\n")
+    exit(1)
+
+
 class MideaPlugin:
     enabled = True
     humUnit = 1
-    tempUnit= 2
+    tempUnit = 2
     runUnit = 3
     fanUnit = 4
-    pumpUnit= 5
-    defrostUnit=6
-    onlineUnit=7
-    targetUnit=8
-    tankUnit=9
-    ionUnit=10
-    sleepUnit=11
-    filterUnit=12
-    tankLevelUnit=13
+    pumpUnit = 5
+    defrostUnit = 6
+    onlineUnit = 7
+    targetUnit = 8
+    tankUnit = 9
+    ionUnit = 10
+    sleepUnit = 11
+    filterUnit = 12
+    tankLevelUnit = 13
 
     def __init__(self):
-       
-        self.cloudLoads = 0;
+        self.cloudLoads = 0
         self.cloud = None
         return
 
     def onStart(self):
         if Parameters["Mode6"] in ("Debug", "File"):
-            Domoticz.Debugging(1);
+            Domoticz.Debugging(1)
 
         Domoticz.Debug("onStart called")
         for key in Parameters:
-           mystr = "param: " + str(key) + '->' + str(Parameters[key])
-           Domoticz.Debug(mystr)
+            mystr = "param: " + str(key) + "->" + str(Parameters[key])
+            Domoticz.Debug(mystr)
 
-        Domoticz.Heartbeat(10);
+        Domoticz.Heartbeat(10)
+        Domoticz.Device(
+            Name="Relative Humidity", Unit=self.humUnit, Type=243, Subtype=6, Used=1
+        ).Create()
+        Domoticz.Device(
+            Name="Temperature", Unit=self.tempUnit, Type=80, Used=1
+        ).Create()
+        Domoticz.Device(
+            Name="Running",
+            Unit=self.runUnit,
+            Type=244,
+            Subtype=73,
+            Switchtype=0,
+            Used=1,
+        ).Create()
+        Domoticz.Device(
+            Name="Fan", Unit=self.fanUnit, Type=243, Subtype=6, Used=1
+        ).Create()
+        Domoticz.Device(
+            Name="Pump", Unit=self.pumpUnit, Type=244, Subtype=73, Switchtype=0, Used=1
+        ).Create()
+        Domoticz.Device(
+            Name="Defrost",
+            Unit=self.defrostUnit,
+            Type=244,
+            Subtype=73,
+            Switchtype=0,
+            Used=1,
+        ).Create()
+        Domoticz.Device(
+            Name="Online",
+            Unit=self.onlineUnit,
+            Type=244,
+            Subtype=73,
+            Switchtype=0,
+            Used=1,
+        ).Create()
+        Domoticz.Device(
+            Name="Humidity Target", Unit=self.targetUnit, Type=243, Subtype=6, Used=1
+        ).Create()
+        Domoticz.Device(
+            Name="Tank Level", Unit=self.tankLevelUnit, Type=243, Subtype=6, Used=1
+        ).Create()
+        Domoticz.Device(
+            Name="Tank Full",
+            Unit=self.tankUnit,
+            Type=244,
+            Subtype=73,
+            Switchtype=0,
+            Used=1,
+        ).Create()
+        Domoticz.Device(
+            Name="Ion Mode",
+            Unit=self.ionUnit,
+            Type=244,
+            Subtype=73,
+            Switchtype=0,
+            Used=1,
+        ).Create()
+        Domoticz.Device(
+            Name="Sleep Mode",
+            Unit=self.sleepUnit,
+            Type=244,
+            Subtype=73,
+            Switchtype=0,
+            Used=1,
+        ).Create()
+        Domoticz.Device(
+            Name="Filter Indicator",
+            Unit=self.filterUnit,
+            Type=244,
+            Subtype=73,
+            Switchtype=0,
+            Used=1,
+        ).Create()
 
-        Domoticz.Device(Name="Relative Humidity", Unit=self.humUnit, Type=243,Subtype=6, Used=1).Create()
-        Domoticz.Device(Name="Temperature", Unit=self.tempUnit, Type=80, Used=1).Create()
-        Domoticz.Device(Name="Running", Unit=self.runUnit, Type=244, Subtype=73, Switchtype=0, Used=1).Create()
-        Domoticz.Device(Name="Fan", Unit=self.fanUnit, Type=243,Subtype=6, Used=1).Create()
-        Domoticz.Device(Name="Pump", Unit=self.pumpUnit, Type=244,Subtype=73,Switchtype=0, Used=1).Create()
-        Domoticz.Device(Name="Defrost", Unit=self.defrostUnit, Type=244,Subtype=73,Switchtype=0, Used=1).Create()
-        Domoticz.Device(Name="Online", Unit=self.onlineUnit, Type=244, Subtype=73, Switchtype=0, Used=1).Create()
-        Domoticz.Device(Name="Humidity Target", Unit=self.targetUnit, Type=243,Subtype=6, Used=1).Create()
-        Domoticz.Device(Name="Tank Level", Unit=self.tankLevelUnit, Type=243,Subtype=6, Used=1).Create()
-        Domoticz.Device(Name="Tank Full", Unit=self.tankUnit, Type=244, Subtype=73, Switchtype=0, Used=1).Create()
-        Domoticz.Device(Name="Ion Mode", Unit=self.ionUnit, Type=244, Subtype=73, Switchtype=0, Used=1).Create()
-        Domoticz.Device(Name="Sleep Mode", Unit=self.sleepUnit, Type=244, Subtype=73, Switchtype=0, Used=1).Create()
-        Domoticz.Device(Name="Filter Indicator", Unit=self.filterUnit, Type=244, Subtype=73, Switchtype=0, Used=1).Create()
- 
         # if (not "Dimmer" in Devices):
         #    Domoticz.Unit(Name="Dimmer", Unit=2, TypeName="Dimmer", DeviceID="Dimmer").Create()
 
     def onStop(self):
         Domoticz.Log("onStop called")
-        Domoticz.Debugging(0);
-        Domoticz.Heartbeat(0);
+        Domoticz.Debugging(0)
+        Domoticz.Heartbeat(0)
 
     def onConnect(self, Connection, Status, Description):
         Domoticz.Log("onConnect called")
@@ -115,11 +182,32 @@ class MideaPlugin:
         Domoticz.Log("onMessage called")
 
     def onCommand(self, Unit, Command, Level, Hue):
-        Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+        Domoticz.Log(
+            "onCommand called for Unit "
+            + str(Unit)
+            + ": Parameter '"
+            + str(Command)
+            + "', Level: "
+            + str(Level)
+        )
 
     def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
-        Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(
-            Priority) + "," + Sound + "," + ImageFile)
+        Domoticz.Log(
+            "Notification: "
+            + Name
+            + ","
+            + Subject
+            + ","
+            + Text
+            + ","
+            + Status
+            + ","
+            + str(Priority)
+            + ","
+            + Sound
+            + ","
+            + ImageFile
+        )
 
     def onDisconnect(self, Connection):
         Domoticz.Log("onDisconnect called")
@@ -128,69 +216,80 @@ class MideaPlugin:
         Domoticz.Log("onHeartbeat called")
 
         try:
-           appliance = self.getDataDirect()
-           myState = appliance.state
+            appliance = self.getDataDirect()
+            myState = appliance.state
 
-           if (myState is not None and myState.current_temperature > 0):
-              self.setValue(MideaPlugin.tempUnit,myState.current_temperature)
-              self.setValue(MideaPlugin.humUnit,myState.current_humidity)
-              self.setValue(MideaPlugin.runUnit,myState.running)
-              self.setValue(MideaPlugin.fanUnit,myState.fan_speed)
-              self.setValue(MideaPlugin.pumpUnit,myState.pump)
-              self.setValue(MideaPlugin.defrostUnit,myState.defrosting)
-              self.setValue(MideaPlugin.onlineUnit,appliance.online)
-              self.setValue(MideaPlugin.targetUnit,myState.target_humidity)
-              self.setValue(MideaPlugin.tankLevelUnit,myState.tank_level)
-              self.setValue(MideaPlugin.tankUnit,myState.tank_full)
-              self.setValue(MideaPlugin.ionUnit,myState.ion_mode)
-              self.setValue(MideaPlugin.sleepUnit,myState.sleep_mode)
-              self.setValue(MideaPlugin.filterUnit,myState.filter_indicator)
+            if myState is not None and myState.current_temperature > 0:
+                self.setValue(MideaPlugin.tempUnit, myState.current_temperature)
+                self.setValue(MideaPlugin.humUnit, myState.current_humidity)
+                self.setValue(MideaPlugin.runUnit, myState.running)
+                self.setValue(MideaPlugin.fanUnit, myState.fan_speed)
+                self.setValue(MideaPlugin.pumpUnit, myState.pump)
+                self.setValue(MideaPlugin.defrostUnit, myState.defrosting)
+                self.setValue(MideaPlugin.onlineUnit, appliance.online)
+                self.setValue(MideaPlugin.targetUnit, myState.target_humidity)
+                self.setValue(MideaPlugin.tankLevelUnit, myState.tank_level)
+                self.setValue(MideaPlugin.tankUnit, myState.tank_full)
+                self.setValue(MideaPlugin.ionUnit, myState.ion_mode)
+                self.setValue(MideaPlugin.sleepUnit, myState.sleep_mode)
+                self.setValue(MideaPlugin.filterUnit, myState.filter_indicator)
         except Exception as err:
-           Domoticz.Error("could not get midea data: "+traceback.format_exc());
-           return
-
+            Domoticz.Error("could not get midea data: " + traceback.format_exc())
+            return
 
     def getCloud(self):
-       try:
-          myCloud = connect_to_cloud(account=Parameters["Mode1"], password=Parameters["Mode2"], appname=Parameters["Mode4"])
-          if (myCloud is None):
-             Domoticz.Error("We've tried to load the cloud "+self.cloudLoads+" times.")
-             return None
-          return myCloud
-       except Exception as err:
-          Domoticz.Error("cloud connect error: "+traceback.format_exc())
-          return None
+        try:
+            myCloud = connect_to_cloud(
+                account=Parameters["Mode1"],
+                password=Parameters["Mode2"],
+                appname=Parameters["Mode4"],
+            )
+            if myCloud is None:
+                Domoticz.Error(
+                    "We've tried to load the cloud " + self.cloudLoads + " times."
+                )
+                return None
+            return myCloud
+        except Exception as err:
+            Domoticz.Error("cloud connect error: " + traceback.format_exc())
+            return None
 
     def getDataDirect(self):
-       if (self.cloud is None):
-          self.cloud = self.getCloud()
-       try:
-          myState = appliance_state(address=Parameters["Mode3"], cloud=self.cloud)
-       except Exception as err:
-          Domoticz.Error("midea state error: "+traceback.format_exc())
-          return None
-       return myState
+        if self.cloud is None:
+            self.cloud = self.getCloud()
+        try:
+            myState = appliance_state(address=Parameters["Mode3"], cloud=self.cloud)
+        except Exception as err:
+            Domoticz.Error("midea state error: " + traceback.format_exc())
+            return None
+        return myState
 
     def setValue(self, Unit, Value):
-       if Value == "True": 
-          sValue = "true"
-          nValue = 1
-       elif Value == "False":
-          sValue = "false"
-          nValue = 0
-       else :
-          Domoticz.Debug("calling setValue: {} for unit: {}".format(str(round(float(Value))), str(Unit)))
-          sValue = str(round(float(Value)))
-          nValue = round(float(Value))
+        if Value == "True":
+            sValue = "true"
+            nValue = 1
+        elif Value == "False":
+            sValue = "false"
+            nValue = 0
+        else:
+            Domoticz.Debug(
+                "calling setValue: {} for unit: {}".format(
+                    str(round(float(Value))), str(Unit)
+                )
+            )
+            sValue = str(round(float(Value)))
+            nValue = round(float(Value))
 
-          Devices[Unit].Update(nValue=nValue,sValue=sValue)
+            Devices[Unit].Update(nValue=nValue, sValue=sValue)
 
 
 _plugin = MideaPlugin()
 _heartbeat_count = 0
- 
+
+
 def onStart():
     _plugin.onStart()
+
 
 def onStop():
     global _plugin
@@ -205,6 +304,7 @@ def onConnect(Connection, Status, Description):
 def onMessage(Connection, Data):
     global _plugin
     _plugin.onMessage(Connection, Data)
+
 
 def onCommand(Unit, Command, Level, Hue):
     global _plugin
@@ -222,7 +322,7 @@ def onDisconnect(Connection):
 
 
 def onHeartbeat():
-    global _plugin,_heartbeat_count
+    global _plugin, _heartbeat_count
 
     # process every 30 heartbeats
     # 10s heartbeats * 30 = 300s
